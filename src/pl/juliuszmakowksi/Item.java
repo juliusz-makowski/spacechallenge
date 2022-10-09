7d8b5d6ac5346cff420465f83f9ac20e9268930b
Juliusz Makowski <julomaki@gmail.com>
2022-10-09 22:32:54 +0200

    created Main and Item class

---
+++ src/pl/juliuszmakowksi/Item.java
@@ -0,0 +1,9 @@
+package pl.juliuszmakowksi;
+
+public class Item {
+    // field
+    String name;
+    int weight;
+
+    // methods
+}
---
+++ src/pl/juliuszmakowksi/Main.java
@@ -0,0 +1,7 @@
+package pl.juliuszmakowksi;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
